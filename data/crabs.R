da4b91700aea85743ac057a58f724759e9d53b5a
dave-harrington <dharrington846@gmail.com>
2024-12-14 14:50:47 -0500

    more files needed for posit

---
+++ data/crabs.R
@@ -0,0 +1,17 @@
+# adding crabs.Rdata to datasets to eliminate dependence on asbio.
+# asbio does not install in current versions of R and R Studio
+
+library(asbio)
+data(crabs)
+crabs <-  crabs |> 
+  mutate(color = fct_recode(color,
+                            "light medium" = "1",
+                            "medium" = "2",
+                            "dark medium" = "3",
+                            "dark" = "4")) |> 
+  mutate(spine = fct_recode(spine,
+                            "both good" = "1",
+                            "one worn or broken" = "2",
+                            "both worn or broken" = "3" )) 
+
+save(crabs, file = "data/crabs.Rdata")
